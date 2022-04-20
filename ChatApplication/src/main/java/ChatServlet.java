3c3a9f5fe222d5058f77ce107bec551605fb7742
Nermeen <nermenelsayed33@gmail.com>
2022-04-20 16:53:40 +0200

    added list of users with their messages in rooms

--- ChatApplication/src/main/java/ChatServlet.java
+++ ChatApplication/src/main/java/ChatServlet.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class Chat
  */
 @WebServlet("/Chat")
-public class Chat extends HttpServlet {
+public class ChatServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	
